47e16d7d6d80cccaac68236ae95fe83779c44265
araz <arazdatas@gmail.com>
2022-12-25 18:04:35 +0300

    initial commit

---
+++ coursework/source.sql
@@ -0,0 +1,56 @@
+-- Создание сущностей
+
+CREATE TABLE adjectives
+( 
+    id BIGSERIAL PRIMARY KEY,
+    ru VARCHAR(200) NULL DEFAULT '',
+    en VARCHAR(200) NULL DEFAULT ''
+);
+
+CREATE TABLE nouns
+(
+    id BIGSERIAL PRIMARY KEY,
+    ru VARCHAR(200) NULL DEFAULT '',
+    en VARCHAR(200) NULL DEFAULT ''
+);
+
+CREATE TABLE others
+( 
+    id BIGSERIAL PRIMARY KEY,
+    ru VARCHAR(200) NULL DEFAULT '',
+    en VARCHAR(200) NULL DEFAULT ''
+);
+
+CREATE TABLE verbs
+( 
+    id BIGSERIAL PRIMARY KEY,
+    ru VARCHAR(200) NULL DEFAULT '',
+    en VARCHAR(200) NULL DEFAULT ''
+);
+
+-- Заполнение данными (см. папку "etc")
+-- PostgreSQL команды
+
+\COPY adjectives(ru, en) FROM '/tmp/ready_adjectives.csv'
+DELIMITER ',' CSV HEADER;
+
+\COPY nouns(ru, en) FROM '/tmp/ready_nouns.csv'
+DELIMITER ',' CSV HEADER;
+
+\COPY others(ru, en) FROM '/tmp/ready_others.csv'
+DELIMITER ',' CSV HEADER;
+
+\COPY verbs(ru, en) FROM '/tmp/ready_verbs.csv'
+DELIMITER ',' CSV HEADER;
+
+CREATE INDEX adjectives_idx_ru ON adjectives(ru);
+CREATE INDEX adjectives_idx_en ON adjectives(en);
+
+CREATE INDEX nouns_idx_ru ON nouns(ru);
+CREATE INDEX nouns_idx_en ON nouns(en);
+
+CREATE INDEX others_idx_ru ON others(ru);
+CREATE INDEX others_idx_en ON others(en);
+
+CREATE INDEX verbs_idx_ru ON verbs(ru);
+CREATE INDEX verbs_idx_en ON verbs(en);
